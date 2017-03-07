6f6ee9ea706607e31984fc56fc67f35e51fc9764
hosford42 <hosford42@gmail.com>
2017-03-06 21:20:36 -0600

    Migrated metadata tags to __about__.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@
 # See LICENSE.txt for licensing information.
 
 import os
+import warnings
 
 from setuptools import setup, find_packages
 
@@ -15,11 +16,16 @@ def get_long_description():
     try:
         # Imported here to avoid creating a dependency in the setup.py
         # if the .rst file already exists.
+
+        # noinspection PyUnresolvedReferences
         from pypandoc.pandoc_download import download_pandoc
+
         download_pandoc()
+
+        # noinspection PyUnresolvedReferences
         from pypandoc import convert_file
     except ImportError:
-        pass
+        warnings.warn("Module pypandoc not installed. Using markdown formatting.")
     else:
         # pandoc, you rock...
         rst_content = convert_file(md_path, 'rst')
@@ -35,20 +41,32 @@ def get_long_description():
             return md_file.read()
 
 
+def get_metadata():
+    about_path = os.path.join(os.path.dirname(__file__), 'vert', '__about__.py')
+    results = {}
+    with open(about_path) as about_file:
+        exec(about_file.read(), results)
+    return results
+
+
+metadata = get_metadata()
+
+
 setup(
-    name='vert',
-    version='0.0.1',
+    name=metadata['__title__'],
+    version=metadata['__version__'],
+    url=metadata['__url__'],
+    license=metadata['__license__'],
+    author=metadata['__author__'],
+    author_email=metadata['__email__'],
+    description=metadata['__summary__'],
+    long_description=get_long_description(),
+
     packages=find_packages(),
     package_data={
         '': ['*.txt', '*.md', '*.rst']
     },
     include_package_data=True,
-    url='https://pypi.python.org/pypi/vert',
-    license='MIT',
-    author='Aaron Hosford',
-    author_email='hosford42@gmail.com',
-    description='Graphs for Python',
-    long_description=get_long_description(),
     keywords='graph vertex edge node link network semantic database',
     classifiers=[
         'Development Status :: 3 - Alpha',
--- vert/__init__.py
+++ vert/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#  Copyright 2017 Aaron M. Hosford
+# Copyright 2017 Aaron M. Hosford
 # See LICENSE.txt for licensing information.
 
 from . import stores, graphs
@@ -10,8 +10,22 @@ from .stores.dbm import DBMGraphStore
 from .stores.memory import MemoryGraphStore
 from .graphs import Graph, Vertex, Edge
 
+from .__about__ import __title__, __summary__, __url__, __version__, __status__, __author__, __maintainer__, \
+    __credits__, __email__, __license__, __copyright__
+
 
 __all__ = [
+    '__title__',
+    '__summary__',
+    '__url__',
+    '__version__',
+    '__status__',
+    '__author__',
+    '__maintainer__',
+    '__credits__',
+    '__email__',
+    '__license__',
+    '__copyright__',
     'stores',
     'graphs',
     'GraphStore',
