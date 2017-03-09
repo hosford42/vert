0d4c0b3e5222af1d2301d890a6825f1ea56824b2
hosford42 <hosford42@gmail.com>
2017-03-08 22:26:41 -0600

    Added docstring

--- vert/__init__.py
+++ vert/__init__.py
@@ -3,6 +3,28 @@
 # Copyright 2017 Aaron M. Hosford
 # See LICENSE.txt for licensing information.
 
+
+"""
+Vert
+====
+
+Vert is a Python package which attempts to provide a standardized interface for graphs. It does so by separating the
+graph into two separate layers of abstraction:
+
+* The graph store: This is where the graph is actually stored and represented. It may be a graph database, another graph
+  library's data structure, or one of vert's built-in graph store objects.
+* The graph interface: This is where you, the programmer, can access the graph via an intuitive object-oriented
+  interface using familiar data types such as Graph, Vertex, and Edge.
+
+Because vert is structured along these distinct layers of abstraction, it is possible to write code that utilizes
+and operates on a graph without regard for the underlying storage mechanisms. Storage mechanisms can be freely swapped
+out for each other at the point where the graph object is initialized, and, aside from differences in performance and
+persistence, the rest of your code will never know the difference. Support for new graph storage mechanisms can be
+added simply by creating a class that supports the GraphStore interface. This means you never have to worry about vendor
+lock-in, and updating your code to use the latest technology is as simple as a one-line change.
+"""
+
+
 from . import stores, graphs
 
 from .stores.base import GraphStore, VertexID, EdgeID, DirectedEdgeID, UndirectedEdgeID, Label
