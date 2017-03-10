d86cdf4b5a738033eb5bd87e728a814cbb91ecac
hosford42 <hosford42@gmail.com>
2017-03-09 18:47:01 -0600

    Export DirectedEdge and UndirectedEdge

--- vert/__init__.py
+++ vert/__init__.py
@@ -30,7 +30,7 @@ from . import stores, graphs
 from .stores.base import GraphStore, VertexID, EdgeID, DirectedEdgeID, UndirectedEdgeID, Label
 from .stores.dbm import DBMGraphStore
 from .stores.memory import MemoryGraphStore
-from .graphs import Graph, Vertex, Edge
+from .graphs import Graph, Vertex, Edge, DirectedEdge, UndirectedEdge
 
 from .__about__ import __title__, __summary__, __url__, __version__, __status__, __author__, __maintainer__, \
     __credits__, __email__, __license__, __copyright__
